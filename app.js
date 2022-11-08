5071fc20d1d86142a72beec995fbdc4fec2e4809
Syed Akif Hussain Zaidi <116806308+AkifZaidi@users.noreply.github.com>
2022-11-09 02:16:01 +0500

    practice
    practice

---
+++ app.js
@@ -0,0 +1,33 @@
+// ARRAY chapter 15 practice
+var onlyArray = ["Pakistan" , "Australia" , "England" , "New zeland" , "Afganistan" , "Natherland"]
+console.log(onlyArray);
+//chapter practice complete
+
+
+// ADDING AND RAMOVING ELEMENT cahpter 16 practice
+onlyArray.push("India")
+console.log(onlyArray);
+
+onlyArray.pop()
+console.log(onlyArray);
+// chapter practice complete
+
+// ARRRAYS REMOVING , INSERTING AND EXTRACTING ELEMENT chapter 17 practice
+onlyArray.shift()
+console.log(onlyArray);
+
+onlyArray.unshift("Iran")
+console.log(onlyArray);
+
+onlyArray.splice(2,3 , "Iraq")
+console.log(onlyArray);
+
+ var js = onlyArray.slice(2,4)
+console.log(onlyArray);
+// chapter practice complete
+
+
+
+for(i = 0 ; i<=50 ; i++){
+    console.log(i)
+}
